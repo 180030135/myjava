27403b0755fc858e2191c9d6b11c2c4f28718a68
180030135 <76677155+180030135@users.noreply.github.com>
2021-01-27 14:08:47 +0530

    Update ADDTWONUMBERS.java

--- ADDTWONUMBERS.java
+++ ADDTWONUMBERS.java
@@ -5,14 +5,7 @@ class Addtwonumbers {
         
         int num1, num2, sum;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Number: ");
-        num1 = sc.nextInt();
-        
-        System.out.println("Enter Second Number: ");
-        num2 = sc.nextInt();
-        
-        sc.close();
-        sum = num1 + num2;
-        System.out.println("Sum of these numbers: "+sum);
-    }
-}
+       num1=1;
+        num2=1;
+        System.out.println(num1+num2);
+    }}
